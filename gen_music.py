4946430d7965c5ae21dbe93c3e82d1d2f2fc7a85
angusseung <angus_seung@outlook.com>
2024-11-12 00:45:07 +0800

    Combiine Music Gen & Image Desc

--- gen_music.py
+++ gen_music.py
@@ -1,8 +1,21 @@
-
 import torch
 import numpy as np
 from diffusers import AudioLDM2Pipeline, DPMSolverMultistepScheduler
 import pyaudio
+from transformers import BlipProcessor, BlipForConditionalGeneration
+from PIL import Image
+import requests
+
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+image = Image.open('./img/plant4.jpg')
+
+inputs = processor(images=image, return_tensors="pt")
+out = model.generate(**inputs)
+caption = processor.decode(out[0], skip_special_tokens=True)
+
+print("Generated Caption:", caption)
 
 pipeline = AudioLDM2Pipeline.from_pretrained(
     "cvssp/audioldm2-music", torch_dtype=torch.float16
@@ -16,7 +29,7 @@ pipeline.enable_model_cpu_offload()
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paFloat32, channels=1, rate=16000, output=True)
 
-audios = pipeline('plant', 
+audios = pipeline(caption, 
         num_inference_steps=200,
         audio_length_in_s=60
     ).audios
