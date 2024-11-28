2e8a319380fc21534553721b35857adadf16e133
angusseung <angus_seung@outlook.com>
2024-11-28 16:28:33 +0800

    Update readme.mm

--- readme.mm
+++ readme.mm
@@ -4,4 +4,4 @@ Discover a unique and engaging way to explore music with Music Album Generator!
 Run in Python 3.11.9
 cd streamlit_web_UI
 pip install -r requirements.txt
-python UI.py
+python -m streamlit run UI.py
