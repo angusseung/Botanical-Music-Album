a178dcd039e5e1cc67246a187a5f48816c3ea7a6
angusseung <angus_seung@outlook.com>
2024-11-28 15:28:22 +0800

    Update readme.mm

--- readme.mm
+++ readme.mm
@@ -1,3 +1,6 @@
+This platform allows you to generate music base on an image.
+Discover a unique and engaging way to explore music with Music Album Generator!
+
 Run in Python 3.11.9
 cd streamlit_web_UI
 pip install -r requirements.txt
